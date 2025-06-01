b3cb3b1c43ec9bbea5cbc06615017a19303c2b54
devbhandari <devendrasinghbhandari@gmail.com>
2025-06-01 12:22:27 +0530

    adding python oops learning

--- myprogram.py
+++ myprogram.py
@@ -1,2 +1,8 @@
-from mymodule import my_func
-my_func()
+# from mymodule import my_func
+# my_func()
+
+from MymainPackage import somemainscript
+from MymainPackage.SubPackage import mysubscript
+# call functions
+somemainscript.somescript_report()
+mysubscript.subscript_report()
