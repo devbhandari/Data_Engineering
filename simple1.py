e95383ab23e65d0dbd210fd7b5f6a70e715fb8a4
devbhandari <devendrasinghbhandari@gmail.com>
2025-06-01 17:17:03 +0530

    adding python oops learning

---
+++ simple1.py
@@ -0,0 +1,14 @@
+'''
+A simple script to test pylint
+'''
+def myfunc():
+    '''
+    A simple function
+    '''
+    first=1
+    second =2
+    print(first)
+    print(second)
+
+# Calling function
+myfunc()
